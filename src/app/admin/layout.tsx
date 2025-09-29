8ec7cfb0fba5fbfac18d45f65707720fc9425081
ecroyd <jcecroyd@gmail.com>
2025-09-29 19:50:04 +0100

    Update layout.tsx

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,6 +1,7 @@
-import { ReactNode } from "react";
-import AdminShellServer from "@/components/admin-shell-server";
-
-export default function AdminLayout({ children }: { children: ReactNode }) {
-  return <AdminShellServer>{children}</AdminShellServer>;
+export default function AdminLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
 }
